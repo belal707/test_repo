a89394fbc5a1f955cf6ca8ff921c6949bb615a79
Belal Ansari <belalansari23@gmail.com>
2017-01-18 11:48:40 +0530

    initial commit

---
+++ src/main/webapp/app/services/auth/register.service.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('welcomeApp')
+        .factory('Register', Register);
+
+    Register.$inject = ['$resource'];
+
+    function Register ($resource) {
+        return $resource('api/register', {}, {});
+    }
+})();
